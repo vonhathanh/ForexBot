22985e653067271ebc3cc23e6dac92c9f82764c6
hanhvn <vonhathanh@gmail.com>
2019-07-21 13:21:13 +0700

    add graph support for displaying training process, not working now

--- env.py
+++ env.py
@@ -1,37 +1,39 @@
 import gym
 import numpy as np
+from sklearn.preprocessing import StandardScaler
 
 from gym import spaces
 
 
-class TradingAgent(gym.Env):
+class TradingEnv(gym.Env):
     metadata = {'render.modes': ['human']}
-    MAX_TRADING_SESSION = 100000
+    MAX_TRADING_SESSION = 3000
     LOT_SIZE = 100000
 
     def __init__(self, df,
                  look_back_window_size=50,
-                 commission=0.001,
+                 commission=0.0003,
                  initial_balance=1000*100,
                  serial=False):
 
-        super(TradingAgent, self).__init__()
+        super(TradingEnv, self).__init__()
         self.df = df.dropna().reset_index()
         self.look_back_window_size = look_back_window_size
         self.initial_balance = initial_balance
         self.commission = commission
         self.serial = serial
+        self.scaler = StandardScaler()
 
-        # TODO: do we need to add buy stop, sell stop, buy limit,
-        #  sell limit to action space? (may be not, start simple first)
-        # action: buy, sell, hold
-        # amount: 0.1, 0.2, 0.5, 1, 2, 5 lot
-        # => 3x6 actions available
-        self.action_space = spaces.MultiDiscrete([3, 6])
-        # observe the OHCL values, networth, and trade history
+        # TODO: do we need to add buy stop, sell stop, buy limit, sell limit to action space? (may be not, start simple first)
+        # action: buy, sell, hold <=> 0, 1, 2
+        # amount: 0.1, 0.2, 0.5, 1, 2, 5 lot (ignore amount for now, we will use 0.5 lot as default
+        # => 3 actions available
+        self.action_space = spaces.Discrete(3)
+        # observe the OHCL values, networth, and trade history (eur, usd held, actions)
+        # TODO: add time to observation space
         self.observation_space = spaces.Box(low=0,
                                             high=1,
-                                            shape=(10, look_back_window_size + 1),
+                                             shape=(8, look_back_window_size + 1),
                                             dtype=np.float16)
 
     def reset(self):
@@ -41,7 +43,7 @@ class TradingAgent(gym.Env):
 
         self.reset_session()
 
-        self.account_history = np.repeat([self.networth, [0], [0], [0], [0]],
+        self.account_history = np.repeat([[self.networth], [0], [self.networth], [0]],
                                          self.look_back_window_size + 1,
                                          axis=1)
         self.trades = []
@@ -55,7 +57,7 @@ class TradingAgent(gym.Env):
             self.steps_left = len(self.df) - self.look_back_window_size - 1
             self.frame_start = self.look_back_window_size
         else:
-            self.steps_left = np.random.randint(1, self.MAX_TRADING_SESSION)
+            self.steps_left = np.random.randint(500, self.MAX_TRADING_SESSION)
             self.frame_start = np.random.randint(self.look_back_window_size, len(self.df) - self.steps_left)
 
         self.active_df = self.df[self.frame_start - self.look_back_window_size : self.frame_start + self.steps_left]
@@ -74,8 +76,11 @@ class TradingAgent(gym.Env):
         obs = np.append(obs, scaled_history[:, -(self.look_back_window_size + 1) :], axis=0)
         return obs
 
+    def get_current_price(self):
+        return self.active_df.iloc[self.current_step].Close
+
     def step(self, action):
-        current_price = self.get_current_price() + self.commission
+        current_price = self.get_current_price()
         self.take_action(action, current_price)
         self.steps_left -= 1
         self.current_step += 1
@@ -86,40 +91,44 @@ class TradingAgent(gym.Env):
             self.reset_session()
 
         obs = self.next_observation()
-        reward = self.networth
+        reward = self.networth - self.prev_networth
         done = self.networth <= 0
 
         return obs, reward, done, {}
 
     def take_action(self, action, current_price):
-        action_type = action[0]
-        amount = action[1]
+        amount = 0.5
 
         buy_price = current_price + self.commission
         sell_price = current_price
 
-        if action_type == 1:  # buy
+        if action == 1:  # buy
             self.eur_held += amount * self.LOT_SIZE
             self.usd_held -= self.eur_held * buy_price
 
-        elif action_type == 2:  # sell
+        elif action == 2:  # sell
             self.eur_held -= amount * self.LOT_SIZE
             self.usd_held += self.eur_held * sell_price
         else:  # hold
             pass
 
+        self.prev_networth = self.networth
         self.networth = self.usd_held + self.eur_held * sell_price
 
-        if action_type == 1 or action_type == 2:
+        if action == 1 or action == 2:
             self.trades.append({'step': self.frame_start + self.current_step,
                                 'amount': amount,
-                                'type': "buy" if action_type == 1 else "sell"})
+                                'type': "buy" if action == 1 else "sell"})
 
         self.account_history = np.append(self.account_history, [
-            self.networth,
-            self.eur_held,
-            self.usd_held,
+            [self.networth],
+            [self.eur_held],
+            [self.usd_held],
+            [action],
         ], axis=1)
+
+    def render(self, mode='human'):
+        print("current netwroth: ", self.networth)
 
 
 
