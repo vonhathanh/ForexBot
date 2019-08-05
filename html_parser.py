2278a4e09028b1419864eb8e5d0c80899d758a6b
hanhvn <hanhvn@bap.jp>
2019-08-05 17:30:07 +0700

    add html_parser helper

---
+++ html_parser.py
@@ -0,0 +1,25 @@
+from html.parser import HTMLParser
+
+
+class ForexNewsParser(HTMLParser):
+
+    def __init__(self):
+        super().__init__()
+        self.dates = set()
+
+    def error(self, message):
+        print("error: ", message)
+
+    def handle_starttag(self, tag, attrs):
+        if tag == 'tr':
+            for attr in attrs:
+                if attr[0] == 'data-event-datetime':
+                    self.dates.add(attr[1])
+
+
+if __name__ == '__main__':
+    with open("./data/Economic Calendar - Investing.com.html", 'r') as f:
+        html_raw = f.read()
+    parser = ForexNewsParser()
+    parser.feed(html_raw)
+    print(sorted(parser.dates))
